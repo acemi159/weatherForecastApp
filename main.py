433446c951e21e255b5da4592a63bfc3e6fd9ffe
acemi159 <61695430+acemi159@users.noreply.github.com>
2024-08-02 15:45:12 +0200

    Update the code

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from backend import get_data
+
 import streamlit as st
 import plotly.express as px
 
@@ -17,14 +19,8 @@ if place and days and option:
     st.subheader(f"{option} for the next {days} in {place}")
 
 # Create the plot
-def get_data(days):
-    
-    dates = ["2022-25-10", "2022-26-10", "2022-27-10"]
-    temperatures = [30, 31, 33]
-    temperatures = [days * i for i in temperatures]
-    return dates, temperatures
+d, t = get_data(place, days, option)
 
-d, t = get_data(days)
 figure = px.line(x=d, y=t, labels={'x':'Date', 'y':'Temperature (C)'})
 
 st.plotly_chart(figure)
