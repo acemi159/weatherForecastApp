430f676d09521bf09b8e9f02f7357a86e2f6d3c8
acemi159 <61695430+acemi159@users.noreply.github.com>
2024-08-02 13:31:56 +0200

    Plot synthetic data dynamically

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import streamlit as st
-import pandas as pd
-
+import plotly.express as px
 
 st.title("Weather Forecast for the Next Days")
 
@@ -17,3 +16,15 @@ option = st.selectbox("Select data to review",
 if place and days and option:
     st.subheader(f"{option} for the next {days} in {place}")
 
+# Create the plot
+def get_data(days):
+    
+    dates = ["2022-25-10", "2022-26-10", "2022-27-10"]
+    temperatures = [30, 31, 33]
+    temperatures = [days * i for i in temperatures]
+    return dates, temperatures
+
+d, t = get_data(days)
+figure = px.line(x=d, y=t, labels={'x':'Date', 'y':'Temperature (C)'})
+
+st.plotly_chart(figure)
